6221371ae4b704c0ae2240c6f0627a717c338ce2
MiguelP <m.mata.pereira@ctt.pt>
2022-08-09 15:25:13 +0100

    mudei setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(
+    name='python-sample-vscode-flask-tutorial',
+    packages=['python-sample-vscode-flask-tutorial'],
+    description='teste',
+    version='0.1',
+    url='https://mmatapereira@dev.azure.com/mmatapereira/mmatapereira/_git/mmatapereira',
+    author='Pereira, Miguel Pereira',
+    author_email='m.mata.pereira@ctt.pt',
+    keywords=['por','favor','funciona']
+)
